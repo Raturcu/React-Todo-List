cc08a4a5046fe7e8cae12051d2e57ea7abf5f85a
Razvan-Andrei Turcu <razvan.andrei.turcu97@gmail.com>
2023-08-28 17:04:07 +0300

    New Commit

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const TodosList = ({todos, setTodos}) => {
+    const handleDelete=({id})=>{
+        setTodos(todos.filter((todo)=>todo.id !==id));
+    }
   return (
     <div>
-        {todos.map((todo)=>(
+        {todos.map((todo) => (
             <li className='list-item' key={todo.id}>
                 <input                
                 type="text"
@@ -15,10 +18,10 @@ const TodosList = ({todos, setTodos}) => {
                     <i className="fa fa-check-circle"></i>
                 </button>
                 <button className="button-edit task-button">
-                    <i className="fa fa-check-circle"></i>
+                    <i className="fa fa-edit"></i>
                 </button>
-                <button className="button-trash task-button">
-                    <i className="fa fa-check-circle"></i>
+                <button className="button-delete task-button" onClick={()=>handleDelete(todo)}>
+                    <i className="fa fa-trash"></i>
                 </button>
             </li>
 
